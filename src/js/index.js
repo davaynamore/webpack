870934a66c9c98dbd388c5bdb8282e44e219b357
Sergey Chuprina <sergey.chuprina@hyuna.bb>
2018-11-20 18:33:50 +0200

    bootstrap added

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,2 @@
 import * as assets from './assets/';
-
-const svg = assets.imgs.svg ? assets.imgs.svg : '';
-
 import makeBigger from './modules/make-bigger';
-
-
-const title = document.createElement('h1');
-document.body.appendChild(title);
-
-title.innerText = 'Hello world';
-
-
-makeBigger(title);
-
-
-const insertSvg = (svg, wrapper) => {
-	const svgWrapper = document.getElementById(wrapper);
-	svgWrapper.innerHTML = svg;
-}
-
-insertSvg(svg, 'logo');
